2cf8c5b2662afa9f374d436b10a8b7e434282115
max power <lifeontherun26.2@gmail.com>
2022-06-15 08:29:30 -0700

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -4,3 +4,8 @@ import datetime
 
 OSCP = datetime.date(2022, 8, 26, 5, 0, 0) - datetime.date.today()
 print(OSCP)
+
+# now = datetime.datetime.now()
+# future = datetime.datetime(2022, 8, 26, 5, 0, 0)
+# OSCP = future - now
+# print(OSCP)
